6e63f37421326979e4bf3452e3fe4d54f57eae75
gaebabja <hippo420@naver.com>
2023-01-10 20:36:55 +0900

    lombok auto DI

--- src/main/java/com/example/shopping/order/OrderServiceImpl.java
+++ src/main/java/com/example/shopping/order/OrderServiceImpl.java
@@ -5,10 +5,12 @@ import com.example.shopping.member.MemberRepository;
 import com.example.shopping.member.MemoryMemberRepository;
 import com.example.shopping.point.NonCrazyPointPolicy;
 import com.example.shopping.point.PointPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -19,15 +21,6 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final PointPolicy pointPolicy;
 
-
-
-    //ComponentScan을 사용하기 때문에 의존성을 자동주입함
-        @Autowired
-        public OrderServiceImpl(MemberRepository memberRepository, PointPolicy pointPolicy) {
-            System.out.println("OrderServiceImpl");
-            this.memberRepository = memberRepository;
-            this.pointPolicy = pointPolicy;
-        }
 
     @Override
     public Order createOrder(String memberId, String itemName, int itemPrice) {
