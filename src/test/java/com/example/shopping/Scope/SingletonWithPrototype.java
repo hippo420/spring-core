c7409d79429b165435c38bd68d1f102bf992d147
gaebabja <hippo420@naver.com>
2023-01-28 01:50:16 +0900

    Provider

--- src/test/java/com/example/shopping/Scope/SingletonWithPrototype.java
+++ src/test/java/com/example/shopping/Scope/SingletonWithPrototype.java
@@ -1,13 +1,16 @@
 package com.example.shopping.Scope;
 
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototype {
 
@@ -47,20 +50,18 @@ public class SingletonWithPrototype {
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
         System.out.println("count2 : "+count2);
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
         ac.close();
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성시점에 같이 주입됨.
-
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private Provider<PrototypeBean> provider; //생성시점에 같이 주입됨.
 
         public int logic(){
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
